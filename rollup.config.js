04ba00a905d3d0ddbf011f7dc370c6d965516d57
Simon Bechmann <sbechmann@hotmail.com>
2022-06-01 21:50:45 +0200

    Proper builds and settings for esm and cjs

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,18 @@ import typescript from '@rollup/plugin-typescript';
 
 export default {
 	input: 'src/main.ts',
-	output: {
-		dir: 'dist',
-		format: 'cjs'
-	},
+	output: [
+    {
+      dir: 'dist',
+      format: 'es',
+      entryFileNames: '[name].js'
+    },
+    {
+      dir: 'dist',
+      format: 'cjs',
+      entryFileNames: '[name].cjs'
+    }
+  ],
 	plugins: [
 		typescript({
 			tsconfig: './tsconfig.json'
